d4a1f43ab30b7d03963be8d489d92747317ca82d
renanleite <renan.vbleite@gmail.com>
2024-01-20 18:27:43 -0300

    Refactoring folder structure

---
+++ api/app.ts
@@ -0,0 +1,5 @@
+import { getStockHandler } from './src/handlers/stock/get-stock';
+import { addItemToStockHandler } from './src/handlers/stock/add-item-stock';
+import { deleteItemFromStockHandler } from './src/handlers/stock/delete-item-stock';
+
+export { getStockHandler, addItemToStockHandler, deleteItemFromStockHandler };
--- src/app.ts
+++
@@ -1,5 +0,0 @@
-import { getStockHandler } from './handlers/stock/get-stock';
-import { addItemToStockHandler } from './handlers/stock/add-item-stock';
-import { deleteItemFromStockHandler } from './handlers/stock/delete-item-stock';
-
-export { getStockHandler, addItemToStockHandler, deleteItemFromStockHandler };
